c2e30f1e8d0b0a994fccf6104fb69802bfd237e5
kongxiong <kxrules@gmail.com>
2016-01-04 16:16:51 -0600

    yup

---
+++ script.js
@@ -0,0 +1,10 @@
+// alert("hello world");
+var button = document.getElementById("button");
+
+// if($('#button').click()){
+// 	alert("hi");
+// };
+
+button.onclick = function() { 
+        alert("blabla");
+    };
